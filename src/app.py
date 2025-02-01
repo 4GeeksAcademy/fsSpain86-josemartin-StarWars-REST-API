32ca058fbb094ae9b869cc2676b4e736fea00eb6
josemartinbm99 <josemartinbm99@gmail.com>
2025-02-01 17:51:58 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Vehicles, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,96 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body= request.get_json()
+    print(request_body)
+    exist = User.query.filter_by(email = request_body["email"]).first()
+    if exist: 
+        return jsonify({
+            "msg":"el usuario ya existe"
+        }), 400
+    new_user = User(email = request_body["email"], password = request_body["password"], is_active = request_body["is_active"])
+    db.session.add(new_user)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "usuario creado"
     }
-
     return jsonify(response_body), 200
+
+
+@app.route('/user', methods=['GET'])
+def get_user():
+    users = User.query.all()
+    
+    serialized_users = list([user.serialize() for user in users])
+    print(serialized_users)
+    if not users:
+        raise APIException("No users found", status_code=404)
+    else:
+        return jsonify(serialized_users), 200
+    
+
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Characters.query.all()
+
+    serialized_characters = list([character.serialize() for character in characters])
+    print(serialized_characters)
+    if not characters:
+        raise APIException("No characters found", status_code=404)
+    else:
+        return jsonify(serialized_characters), 200
+    
+
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_one_person(id):
+    person = Characters.query.get(id)
+    if person is None: 
+        return jsonify({"msg": "no character found"})
+    return jsonify(person.serialize()), 200
+    
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    serialized_planets = list([planet.serialize() for planet in planets])
+    print(serialized_planets)
+    if not planets:
+        raise APIException("No planets found", status_code=404)
+    else:
+        return jsonify(serialized_planets), 200
+    
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    planet = Planets.query.get(id)
+    if planet is None: 
+        return jsonify({"msg": "no planet found"})
+    return jsonify(planet.serialize()), 200
+    
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicles.query.all()
+    serialized_vehicles = list([vehicle.serialize() for vehicle in vehicles])
+    print(serialized_vehicles)
+    if not vehicles:
+        raise APIException("No vehicles found", status_code=404)
+    else:
+        return jsonify(serialized_vehicles), 200
+
+
+@app.route('/vehicles/<int:id>', methods=['GET'])
+def get_one_vehicle(id):
+    vehicle = Vehicles.query.get(id)
+    if vehicle is None: 
+        return jsonify({"msg": "no vehicle found"})
+    return jsonify(vehicle.serialize()), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,90 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    height = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+    poblation = db.Column(db.String(250))
+    orbital_period = db.Column(db.String(250))
+    rotation_period = db.Column(db.String(250))
+    diameter = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "poblation": self.poblation,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    pilots = db.Column(db.String(250))
+    cargo_capacity = db.Column(db.String(250))
+    crew = db.Column(db.String(250))
+    model = db.Column(db.String(250))
+    consumables = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "pilots": self.pilots,
+            "cargo_capacity": self.cargo_capacity,
+            "crew": self.crew,
+            "model": self.model,
+            "consumables": self.consumables,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    characters = db.relationship(Characters)
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    vehicles = db.relationship(Vehicles)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planets = db.relationship(Planets)
